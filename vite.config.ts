88d27cadd04ddb6078f1560e7ccb54cc8edf2e09
SOV710 <chris916911179@outlook.com>
2025-04-13 07:00:38 +0800

    rewrite vite.config.rs, add sass support

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 import Components from 'unplugin-vue-components/vite'
 import VueRouter from 'unplugin-vue-router/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -19,15 +20,35 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
     AutoImport({
+      imports: [
+        'vue',
+        VueRouterAutoImports,
+      ],
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [ElementPlusResolver({
+        importStyle: 'sass',
+      })],
     }),
   ],
+
   resolve: {
     alias: {
+      '~': fileURLToPath(new URL('./src', import.meta.url)),
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/styles/element/index.scss" as *;`,
+        api: 'modern-compiler',
+      },
+    },
+  },
+
 })
